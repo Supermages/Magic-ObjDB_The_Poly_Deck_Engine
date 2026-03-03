9b499296702ca048e58d0ee724106deb2f537ce8
Marc Sastre Cabré <masa863@vidalibarraquer.net>
2026-03-03 09:53:33 +0100

    Add Criatura

--- src/main/java/com/mycompany/magic/objdb_the_poly_deck_engine/model/Criatura.java
+++ src/main/java/com/mycompany/magic/objdb_the_poly_deck_engine/model/Criatura.java
@@ -1,5 +1,52 @@
 package com.mycompany.magic.objdb_the_poly_deck_engine.model;
 
+import com.mycompany.magic.objdb_the_poly_deck_engine.model.enums.Rareza;
+
 public class Criatura extends Carta{
+    private int strength;
+    private int resistence;
+    private String type;
+    private boolean flying;
+
+    public Criatura(int strength, int resistence, String type, boolean flying, String nom, String descripcio, Rareza rareza, String edicion, CosteMana coste) {
+        super(nom, descripcio, rareza, edicion, coste);
+        this.strength = strength;
+        this.resistence = resistence;
+        this.type = type;
+        this.flying = flying;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getResistence() {
+        return resistence;
+    }
+
+    public void setResistence(int resistence) {
+        this.resistence = resistence;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isFlying() {
+        return flying;
+    }
+
+    public void setFlying(boolean flying) {
+        this.flying = flying;
+    }
+    
     
 }
